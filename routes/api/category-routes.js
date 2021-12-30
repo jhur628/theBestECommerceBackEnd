874edd7ec07261910c793f0402ca867bbed9baa7
John Hur <hur.john628@gmail.com>
2021-12-30 00:49:15 -0600

    category route delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -62,8 +62,21 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'Cannot delete category!' })
+    }
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json('OOPS! You are suck.');
+  }
 });
 
 module.exports = router;
