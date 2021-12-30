86dd0ee0ab5e2aa5cbf18f74c8b625247df2aa7d
John Hur <hur.john628@gmail.com>
2021-12-30 01:30:39 -0600

    product route delete?

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -113,6 +113,19 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'Cannot delete product!' })
+    };
+    res.status(200).json(productData)
+  } catch (err) {
+    res.status(500).json('OOPS! You are suck. ' + err);
+  }
 });
 
 module.exports = router;
